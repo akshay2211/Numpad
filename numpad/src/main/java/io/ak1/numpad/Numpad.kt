aa1217ec1d97694bb3a0255c824d6d7b56f9b41a
Akshay Sharma <fxn769@gmail.com>
2021-09-20 23:15:19 +0530

    compose Numpad component added

---
+++ numpad/src/main/java/io/ak1/numpad/Numpad.kt
@@ -0,0 +1,16 @@
+package io.ak1.numpad
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * Created by akshay on 20/09/21
+ * https://ak1.io
+ */
+
+@Composable
+fun Numpad(
+    modifier: Modifier = Modifier,
+){
+
+}
