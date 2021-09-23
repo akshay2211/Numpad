803629ec9c9e376f6327a226b01b54b121cacacb
Akshay Sharma <fxn769@gmail.com>
2021-09-23 23:35:29 +0530

    multiple features added

--- numpad/src/main/java/io/ak1/numpad/Components.kt
+++ numpad/src/main/java/io/ak1/numpad/Components.kt
@@ -2,12 +2,14 @@ package io.ak1.numpad
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -19,24 +21,34 @@ import androidx.compose.ui.unit.dp
 
 
 @Composable
-fun TextBox(modifier: Modifier, text: String, callback: (type: NumType, number: Int) -> Unit) {
+fun TextBox(modifier: Modifier, text: String,configuration: Configuration, callback: (type: NumType, number: Int) -> Unit) {
     Button(
         modifier = modifier, onClick = {
-            callback(NumType.NUMBER,text.toInt())
+            callback(NumType.NUMBER, text.toInt())
         },
         colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.surface),
         elevation = ButtonDefaults.elevation(defaultElevation = 0.dp)
     ) {
-        Text(text = text, textAlign = TextAlign.Center)
+        Text(text = text, fontFamily = configuration.fontFamily,fontSize = configuration.fontSize,textAlign = TextAlign.Center)
     }
 }
 
 
 @Composable
-fun TextRow(modifierRow: Modifier, contentModifier: Modifier, range: IntRange,callback: (type: NumType, number: Int) -> Unit) {
+fun TextRow(
+    modifierRow: Modifier,
+    configuration: Configuration,
+    range: IntRange,
+    callback: (type: NumType, number: Int) -> Unit
+) {
     Row(modifier = modifierRow) {
         for (i in range) {
-            TextBox(text = i.toString(), modifier = contentModifier,callback = callback)
+            TextBox(
+                text = i.toString(),
+                modifier = configuration.inner_modifier.weight(1f, true),
+                configuration = configuration,
+                callback = callback
+            )
         }
     }
 }
@@ -44,24 +56,34 @@ fun TextRow(modifierRow: Modifier, contentModifier: Modifier, range: IntRange,ca
 @Composable
 fun CustomRow(
     modifierRow: Modifier,
-    contentModifier: Modifier,
-    allowDecimal: Boolean,
-    backPressIcon: Int,
+    configuration: Configuration,
     callback: (type: NumType, number: Int) -> Unit
 ) {
-    val decimalText = if (allowDecimal) "." else " "
     Row(modifier = modifierRow) {
-        TextBox(text = decimalText, modifier = contentModifier,callback = callback)
-        TextBox(text = "0", modifier = contentModifier,callback = callback)
+        TextBox(
+            text = " ",
+            modifier = configuration.inner_modifier.weight(1f, true),
+            configuration = configuration,
+            callback = callback
+        )
+        TextBox(
+            text = "0",
+            modifier = configuration.inner_modifier.weight(1f, true),
+            configuration = configuration,
+            callback = callback
+        )
         Button(
-            shape = MaterialTheme.shapes.small,
-            modifier = contentModifier, onClick = {
-                callback(NumType.DELETE,0)
+            shape = RoundedCornerShape(configuration.cornerRadius),
+            modifier = configuration.inner_modifier.weight(1f, true), onClick = {
+                callback(NumType.DELETE, 0)
             },
             colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.surface),
             elevation = ButtonDefaults.elevation(defaultElevation = 0.dp)
         ) {
-            Image(painter = painterResource(backPressIcon), contentDescription = "backpress")
+            Image(
+                painter = painterResource(configuration.deleteIcon),
+                contentDescription = "delete_icon"
+            )
         }
     }
 }
